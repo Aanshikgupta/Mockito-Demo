dd39ce73aaace4670ba0896c59e267ef42e9dca8
Aanshik Gupta <aanshik.gupta.in@freecharge.com>
2023-03-11 13:57:52 +0530

    Mockito Mock implemented, V.Imp

--- src/test/java/com/aanshik/mockitodemo/MockTest.java
+++ src/test/java/com/aanshik/mockitodemo/MockTest.java
@@ -2,25 +2,33 @@ package com.aanshik.mockitodemo;
 
 import com.aanshik.mockitodemo.Service.DataService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class MockTest {
 
+    @InjectMocks
+    DataUsage dataUsage;
+    @Mock
     DataService dataService;
     @Test
     void dataUsageTest(){
 
         //create a mock
-        dataService=mock(DataService.class);
+        //dataService=mock(DataService.class);
 
         //provide data using mockito mock methods
         //v. v. easy
         when(dataService.retrieveAllData()).thenReturn(new int[]{1,2,3,4});
 
-        DataUsage dataUsage=new DataUsage(dataService);
+        //DataUsage dataUsage=new DataUsage(dataService);
         int x=dataUsage.greatestOfAll();
 
         assertEquals(x,4);
